ed8ceaa0f12e76bca087d6e0afbe3880173b3210
albim72 <marcin.albiniak72@gmail.com>
2023-03-20 10:16:12 +0100

    Create kolekcje.py

---
+++ DZIEN_1/kolekcje.py
@@ -0,0 +1,13 @@
+print("kolekcje języka python")
+#zwykły komentarz
+"""
+komemntarz wieloliniowy
+dokumentacyjny
+"""
+
+"""
+komemntarz wieloliniowy
+nie dokumentacyjny
+"""
+#kolekcja -> listy
+imiona = ["Jan","Leon","Olga","Tytus","Roma","Edyta","Jan","Olga","Jan"]
