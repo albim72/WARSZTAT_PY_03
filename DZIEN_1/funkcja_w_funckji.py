9f0c9d23da7fa79e76bf33c58c8a9e7c99f68027
albim72 <marcin.albiniak72@gmail.com>
2023-03-20 13:37:40 +0100

    Update funkcja_w_funckji.py

--- DZIEN_1/funkcja_w_funckji.py
+++ DZIEN_1/funkcja_w_funckji.py
@@ -12,3 +12,26 @@ print(konto(user,"Marta"))
 print(konto(przelew,"Leon",13200,"2023-03-10"))
 
 #przykład 2
+def przejazd(oplata):
+    def _bilet():
+        return "kontrola biletów: OK"
+
+    def _brak():
+        return "kontrola biletów: kara finansowa"
+
+    def _error():
+        return "błąd odczytu kodu biletu. Spróbuj ponownie (1-opłacony, 0-brak)"
+
+    if oplata == 1:
+        return _bilet
+    elif oplata == 0:
+        return _brak
+    else:
+        return _error
+
+print(przejazd(1)())
+print(przejazd(0)())
+print(przejazd(-3)())
+
+
+
