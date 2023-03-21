cc4082e0dee4c05d2204fbb0f0b85de18a450394
albim72 <marcin.albiniak72@gmail.com>
2023-03-21 15:01:26 +0100

    Update tyrion.py

--- DZIEN_2/game_of_throne/tyrion.py
+++ DZIEN_2/game_of_throne/tyrion.py
@@ -3,18 +3,18 @@ from czlonekrodu import CzlonekRodu
 class Tyrion(CzlonekRodu):
     def walka(self):
         if self.punkty_walki>self.punkty_palacowe and self.punkty_walki>=6:
-            print("Lord wysłany do walki...")
+            print(f"{self.tytul} wysłany do walki...")
         else:
-            print("Lord zostaje w pałacu")
+            print(f"{self.tytul} zostaje w pałacu")
 
     def negocjacja(self):
         if self.punkty_palacowe >= 8:
-            print("Lord wysłany do negocjacji...")
+            print(f"{self.tytul} wysłany do negocjacji...")
         else:
-            print("Lord wysłany do walki...")
+            print(f"{self.tytul} wysłany do walki...")
 
     def uczta(self):
         if self.punkty_palacowe >= 6 and self.doswiadczenie >= 7:
-            print("Lord jedzie na ucztę...")
+            print(f"{self.tytul} jedzie na ucztę...")
         else:
-            print("Lord pozostaje w dyspozycji królowej...")
+            print(f"{self.tytul} pozostaje w dyspozycji królowej...")
