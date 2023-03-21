77b191915f907baccc9cde93aca1a33580250f3e
albim72 <marcin.albiniak72@gmail.com>
2023-03-21 09:30:48 +0100

    Update main.py

--- DZIEN_2/mojemoduly/main.py
+++ DZIEN_2/mojemoduly/main.py
@@ -3,6 +3,7 @@
 from dane import nr_filii as filia
 from dane import book as ks
 from funkcje.bfunkcje import czytaj_liste, czytaj_slownik
+from klasy.cklasa import CDane
 
 print("________ wyświetlanie bezpośrednie ____________")
 print(filia)
@@ -11,3 +12,8 @@ print(ks)
 print("________ wyświetlanie za pomocą funkcji ____________")
 czytaj_liste(filia)
 czytaj_slownik(ks)
+
+print("________ wyświetlanie za pomocą obiektu klasy ____________")
+cd = CDane(filia,ks)
+cd.czytaj_l()
+cd.czytaj_s()
