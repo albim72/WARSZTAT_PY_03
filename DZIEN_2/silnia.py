73c98f7dfb0d3b9c6b4335cbbd7b106142c8034c
albim72 <marcin.albiniak72@gmail.com>
2023-03-21 12:29:09 +0100

    Update silnia.py

--- DZIEN_2/silnia.py
+++ DZIEN_2/silnia.py
@@ -1,5 +1,7 @@
 import sys
 
+import time
+
 
 sys.set_int_max_str_digits(0x1000000)
 sys.setrecursionlimit(0x1000000)
@@ -21,7 +23,13 @@ def silnia_rek(n):
 
 try:
     n = int(input("podaj wartość argumentu n funkcji silnia: "))
+    s = time.perf_counter()
     print(f"silnia z wartości {n} wynosi: {silnia(n)}")
+    k = time.perf_counter()
+    print(f'czas wykonania iteracji: {k-s} s')
+    s = time.perf_counter()
     print(f"silnia rekurencyjna z wartości {n} wynosi: {silnia_rek(n)}")
+    k = time.perf_counter()
+    print(f'czas wykonania rekurencji: {k - s} s')
 except ValueError as v:
     print(v)
